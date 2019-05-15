65eb1e36a518d81c4ab8ee0d8018f87ba764a242
okra33001581 <okra33001581@163.com>
2019-05-15 15:15:28 +0800

    add

--- operation_profit.go
+++ operation_profit.go
@@ -13,21 +13,52 @@ import (
 )
 
 type Tweet struct {
-	Id            string `json:"id`
-	Sub_account   string `json:"sub_account`
-	Operate_name  string `json:"operate_name`
-	Log_content   string `json:"log_content`
-	Ip            string `json:"ip`
-	Cookies       string `json:"cookies`
-	Date          string `json:"date`
-	Merchant_id   string `json:"merchant_id`
-	Merchant_name string `json:"merchant_name`
-	Created_at    string `json:"created_at`
-	Origin        string `json:"origin`
-	Referer       string `json:"referer`
-	User_agent    string `json:"user_agent`
-	Type          string `json:"type`
+            Id string`json:"id`
+            Merchant_id string`json:"merchant_id`
+            Merchant_name string`json:"merchant_name`
+            User_id string`json:"user_id`
+            Username string`json:"username`
+            Group string`json:"group`
+            In_total_amount string`json:"in_total_amount`
+            Total_out_amount string`json:"total_out_amount`
+            Valid_profit string`json:"valid_profit`
+            Sum_turnover string`json:"sum_turnover`
+            Prize_amount string`json:"prize_amount`
+            Rebate_amount string`json:"rebate_amount`
+            Game_profit_loss string`json:"game_profit_loss`
+            Benefit_amount string`json:"benefit_amount`
+            Day_salary string`json:"day_salary`
+            System_subtraction string`json:"system_subtraction`
+            Final_amount string`json:"final_amount`
+            Date string`json:"date`
+            Platform string`json:"platform`
+            Model string`json:"model`
 }
+
+id
+merchant_id
+merchant_name
+user_id
+username
+group
+in_total_amount
+total_out_amount
+valid_profit
+sum_turnover
+prize_amount
+rebate_amount
+game_profit_loss
+benefit_amount
+day_salary
+system_subtraction
+final_amount
+date
+platform
+model
+
+
+
+
 
 func main() {
 	client, err := elastic.NewClient(elastic.SetURL("http://192.168.36.147:9200"))
@@ -39,16 +70,16 @@ func main() {
 
 	checkErr(err)
 
-	rows, err := db.Query("SELECT id, type, sub_account,operate_name,log_content,ip,cookies,date,merchant_id,merchant_name,created_at,origin,referer,user_agent FROM log_admin")
+	rows, err := db.Query("SELECT id,merchant_id,merchant_name,user_id,username,group,in_total_amount,total_out_amount,valid_profit,sum_turnover,prize_amount,rebate_amount,game_profit_loss,benefit_amount,day_salary,system_subtraction,final_amount,date,platform,model FROM log_admin")
 	checkErr(err)
 	bulkRequest := client.Bulk()
 	for rows.Next() {
-		var id, type1, sub_account, operate_name, log_content, ip, cookies, date, merchant_id, merchant_name, created_at, origin, referer, user_agent string
-		if err := rows.Scan(&id, &type1, &sub_account, &operate_name, &log_content, &ip, &cookies, &date, &merchant_id, &merchant_name, &created_at, &origin, &referer, &user_agent); err == nil {
+		var id,merchant_id,merchant_name,user_id,username,group,in_total_amount,total_out_amount,valid_profit,sum_turnover,prize_amount,rebate_amount,game_profit_loss,benefit_amount,day_salary,system_subtraction,final_amount,date,platform,model string
+		if err := rows.Scan(&id,&merchant_id,&merchant_name,&user_id,&username,&group,&in_total_amount,&total_out_amount,&valid_profit,&sum_turnover,&prize_amount,&rebate_amount,&game_profit_loss,&benefit_amount,&day_salary,&system_subtraction,&final_amount,&date,&platform,&model); err == nil {
 			fmt.Println(err)
 		}
 
-		tweet := Tweet{Id: id, Sub_account: sub_account, Operate_name: operate_name, Log_content: log_content, Ip: ip, Cookies: cookies, Date: date, Merchant_id: merchant_id, Merchant_name: merchant_name, Created_at: created_at, Origin: origin, Referer: referer, User_agent: user_agent, Type: type1}
+		tweet := Tweet{Id:id,Merchant_id:merchant_id,Merchant_name:merchant_name,User_id:user_id,Username:username,Group:group,In_total_amount:in_total_amount,Total_out_amount:total_out_amount,Valid_profit:valid_profit,Sum_turnover:sum_turnover,Prize_amount:prize_amount,Rebate_amount:rebate_amount,Game_profit_loss:game_profit_loss,Benefit_amount:benefit_amount,Day_salary:day_salary,System_subtraction:system_subtraction,Final_amount:final_amount,Date:date,Platform:platform,Model:model,}
 		// req := elastic.NewBulkIndexRequest().Index("log_admin").Type("log_admin").Id(id).Doc(tweet)
 		req := elastic.NewBulkIndexRequest().Index("log_admin1").Type("log_admin1").Id(id).Doc(tweet)
 		bulkRequest = bulkRequest.Add(req)
