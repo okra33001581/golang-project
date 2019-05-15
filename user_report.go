d220c068c743a4dc88907e541bd7750b30b435e8
okra33001581 <okra33001581@163.com>
2019-05-15 15:28:14 +0800

    add

--- user_report.go
+++ user_report.go
@@ -13,21 +13,35 @@ import (
 )
 
 type Tweet struct {
-	Id            string `json:"id`
-	Sub_account   string `json:"sub_account`
-	Operate_name  string `json:"operate_name`
-	Log_content   string `json:"log_content`
-	Ip            string `json:"ip`
-	Cookies       string `json:"cookies`
-	Date          string `json:"date`
-	Merchant_id   string `json:"merchant_id`
-	Merchant_name string `json:"merchant_name`
-	Created_at    string `json:"created_at`
-	Origin        string `json:"origin`
-	Referer       string `json:"referer`
-	User_agent    string `json:"user_agent`
-	Type          string `json:"type`
+            Id string`json:"id`
+            Date string`json:"date`
+            Ip_count string`json:"ip_count`
+            Register_count string`json:"register_count`
+            Active_count string`json:"active_count`
+            First_deposit_count string`json:"first_deposit_count`
+            First_deposit_amount string`json:"first_deposit_amount`
+            In_people_count string`json:"in_people_count`
+            In_times string`json:"in_times`
+            Out_times string`json:"out_times`
+            Merchant_id string`json:"merchant_id`
+            Merchant_name string`json:"merchant_name`
+            Platform string`json:"platform`
+            Model string`json:"model`
 }
+id
+date
+ip_count
+register_count
+active_count
+first_deposit_count
+first_deposit_amount
+in_people_count
+in_times
+out_times
+merchant_id
+merchant_name
+platform
+model
 
 func main() {
 	client, err := elastic.NewClient(elastic.SetURL("http://192.168.36.147:9200"))
@@ -39,16 +53,16 @@ func main() {
 
 	checkErr(err)
 
-	rows, err := db.Query("SELECT id, type, sub_account,operate_name,log_content,ip,cookies,date,merchant_id,merchant_name,created_at,origin,referer,user_agent FROM log_admin")
+	rows, err := db.Query("SELECT id,date,ip_count,register_count,active_count,first_deposit_count,first_deposit_amount,in_people_count,in_times,out_times,merchant_id,merchant_name,platform,model FROM log_admin")
 	checkErr(err)
 	bulkRequest := client.Bulk()
 	for rows.Next() {
-		var id, type1, sub_account, operate_name, log_content, ip, cookies, date, merchant_id, merchant_name, created_at, origin, referer, user_agent string
-		if err := rows.Scan(&id, &type1, &sub_account, &operate_name, &log_content, &ip, &cookies, &date, &merchant_id, &merchant_name, &created_at, &origin, &referer, &user_agent); err == nil {
+		var id,date,ip_count,register_count,active_count,first_deposit_count,first_deposit_amount,in_people_count,in_times,out_times,merchant_id,merchant_name,platform,model string
+		if err := rows.Scan(&id,&date,&ip_count,&register_count,&active_count,&first_deposit_count,&first_deposit_amount,&in_people_count,&in_times,&out_times,&merchant_id,&merchant_name,&platform,&model); err == nil {
 			fmt.Println(err)
 		}
 
-		tweet := Tweet{Id: id, Sub_account: sub_account, Operate_name: operate_name, Log_content: log_content, Ip: ip, Cookies: cookies, Date: date, Merchant_id: merchant_id, Merchant_name: merchant_name, Created_at: created_at, Origin: origin, Referer: referer, User_agent: user_agent, Type: type1}
+		tweet := Tweet{Id:id,Date:date,Ip_count:ip_count,Register_count:register_count,Active_count:active_count,First_deposit_count:first_deposit_count,First_deposit_amount:first_deposit_amount,In_people_count:in_people_count,In_times:in_times,Out_times:out_times,Merchant_id:merchant_id,Merchant_name:merchant_name,Platform:platform,Model:model}
 		// req := elastic.NewBulkIndexRequest().Index("log_admin").Type("log_admin").Id(id).Doc(tweet)
 		req := elastic.NewBulkIndexRequest().Index("log_admin1").Type("log_admin1").Id(id).Doc(tweet)
 		bulkRequest = bulkRequest.Add(req)
